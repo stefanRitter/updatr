c86162154a6f09a9d9f2a05346b3b1dfc2d7c202
Stefan Ritter <stefan.ritter@sap.com>
2017-08-23 11:13:57 +0200

    use alarms to tehck for updates

--- chrome/chrome-button.js
+++ chrome/chrome-button.js
@@ -1,6 +1,4 @@
 
-// TODO: check for updates in sync with the cronjob's schedule
-
 var domain = 'https://www.getupdatr.com';
 
 // update link count
@@ -21,19 +19,7 @@ function udpateLinkCount (uid) {
 }
 
 
-// opens a new tab with Updatr when clicked and updates unread links count
-chrome.browserAction.onClicked.addListener(function () {
-    chrome.cookies.get({url: domain, name: 'uid'}, function (cookie) {
-        if (!cookie) {
-            window.open(domain+'/login','_blank');
-            chrome.browserAction.setBadgeText({text: ''});
-        } else {
-            udpateLinkCount(cookie.value);
-            window.open(domain,'_blank');
-        }
-    });
-});
-
+// 1
 // check if logged in on browser open
 chrome.cookies.get({"url": domain, "name": 'uid'}, function (cookie) {
     if (!cookie) {
@@ -43,3 +29,37 @@ chrome.cookies.get({"url": domain, "name": 'uid'}, function (cookie) {
         udpateLinkCount(cookie.value);
     }
 });
+
+
+// 2
+// opens a new tab with Updatr when clicked and updates unread links count
+chrome.browserAction.onClicked.addListener(function () {
+    chrome.cookies.get({url: domain, name: 'uid'}, function (cookie) {
+        if (!cookie) {
+            window.open(domain+'/login','_blank');
+            chrome.browserAction.setBadgeText({text: ''});
+        } else {
+            // after activating check after every active change for 15 min
+            var updateListener = udpateLinkCount.bind(this, cookie.value);
+            chrome.tabs.onActivated.addListener(updateListener);
+
+            setTimeout(function () {
+                chrome.tabs.onActivated.removeListener(updateListener);
+            }, 150000);
+
+            window.open(domain,'_blank');
+        }
+    });
+});
+
+
+// 3
+// check for new count every hour
+chrome.alarms.onAlarm.addListener(function (alarm) {
+    chrome.cookies.get({"url": domain, "name": 'uid'}, function (cookie) {
+        if (cookie) {
+            udpateLinkCount(cookie.value);
+        }
+    });
+});
+chrome.alarms.create('updatrRefresh', {periodInMinutes: 60});
